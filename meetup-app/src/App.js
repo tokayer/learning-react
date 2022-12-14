a8061cb05e21cb0c6f56a3efd8692050285439ec
tokayer <yltokayer@gmail.com>
2022-12-14 07:46:50 +0200

    reorganizing folders

---
+++ meetup-app/src/App.js
@@ -0,0 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
+function App() {
+  return <div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  </div>;
+}
+
+export default App;
