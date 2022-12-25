a4f9832a9adb9725604882c9d8ea81b25857f2e7
tokayer <yltokayer@gmail.com>
2022-12-25 09:05:20 +0200

    add new expense form

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -2,15 +2,27 @@ import './Expenses.css';
 import './ExpenseItem';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
+import { useState } from 'react';
 
 function Expenses(props) {
+  const [filterValue, setFilterValue] = useState('2020');
+
+  const onFilterHandler = value => {
+    setFilterValue(value);
+    console.log(value);
+  }
+
   return (
-    <Card className="expenses">   
-      <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-      <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-      <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-      <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filterValue} onFilter={onFilterHandler} />
+        <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+        <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
+        <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
+        <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+      </Card>
+    </div>
   );
 }
 
