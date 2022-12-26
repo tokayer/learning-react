c4c4b5d06720a5e8cfd9bf30d2b8b1ea821574ce
tokayer <yltokayer@gmail.com>
2022-12-26 08:25:21 +0200

    displaying a list

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -1,26 +1,26 @@
 import './Expenses.css';
 import './ExpenseItem';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
   const [filterValue, setFilterValue] = useState('2020');
 
   const onFilterHandler = value => {
     setFilterValue(value);
-    console.log(value);
   }
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterValue;
+  });
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filterValue} onFilter={onFilterHandler} />
-        <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-        <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-        <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-        <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
